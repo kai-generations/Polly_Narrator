b5df8585cd6e65ddf8f9814bfb838fa8f1c787b8
kai-generations <kai.newgenerations@gmail.com>
2026-01-25 09:47:40 -0500

    Saving added resoruces

--- main.tf
+++ main.tf
@@ -1,18 +1,33 @@
-provider aws {
-    region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
 
 data "aws_iam_policy_document" "Polly_Full_Access" {
-    statement {
-        effect = "Allow"
-      actions = ["polly:*"]
-      resources = ["*"]
-    }
+  statement {
+    effect    = "Allow"
+    actions   = ["polly:*"]
+    resources = ["*"]
+  }
 }
 
 data "aws_iam_policy_document" "S3_Full_Access" {
-    statement {
-      effect = "Allow"
-      actions = ["*"]
-    }
+  statement {
+    effect = "Allow"
+    actions = ["s3:*",
+      "s3-object-lambda:*"
+    ]
+    resources = ["*"]
+  }
 }
+
+data "aws_iam_policy_document" "AWS_Lambda_Basic_ExecutionRole" {
+  statement {
+    effect = "Allow"
+    actions = ["logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }
+}
+
