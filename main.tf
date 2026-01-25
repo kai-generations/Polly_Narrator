8cebc7ee4015aa7ee0dc846e02dfd88fac4c2eef
kai-generations <kai.newgenerations@gmail.com>
2026-01-25 10:01:03 -0500

    modified branch

--- main.tf
+++ main.tf
@@ -1,33 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-
-data "aws_iam_policy_document" "Polly_Full_Access" {
-  statement {
-    effect    = "Allow"
-    actions   = ["polly:*"]
-    resources = ["*"]
-  }
-}
-
-data "aws_iam_policy_document" "S3_Full_Access" {
-  statement {
-    effect = "Allow"
-    actions = ["s3:*",
-      "s3-object-lambda:*"
-    ]
-    resources = ["*"]
-  }
-}
-
-data "aws_iam_policy_document" "AWS_Lambda_Basic_ExecutionRole" {
-  statement {
-    effect = "Allow"
-    actions = ["logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    resources = ["*"]
-  }
-}
-
