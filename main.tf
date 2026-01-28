0e244fff90dba7f82173d794dbe5d16ccd9b0630
kai-generations <kai.newgenerations@gmail.com>
2026-01-28 18:12:04 -0500

    added archive file block, s3 bucket block, and lambda bucket block

--- main.tf
+++ main.tf
@@ -11,8 +11,21 @@ locals {
   ]
 }
 
+# This block will package the Lambda function code; will require archive provider
+data "archive_file" "javascript_polly_lambda" {
+  type = "zip"
+  source_file = "${path.module}/textToSpeech.js" # ${path.module} is a dynamic pointer that always identifies the filesystem path of the specific folder where the .tf files be written are
+  output_path = "${path.module}/textToSpeech.zip"
+}
+
+# This block will create an S3 bucket
+resource "aws_s3_bucket" "polly_audio_storage" {
+  region = "us-east-1"
+  force_destroy = true
+}
+
 # This resource creates the lambda role 
-resource "aws_iam_role" "lamda_role" {
+resource "aws_iam_role" "lambda_role" {
   name                  = "PollyTranslationRole"
   force_detach_policies = true
   assume_role_policy = jsonencode({
@@ -33,6 +46,15 @@ resource "aws_iam_role" "lamda_role" {
 resource "aws_iam_role_policy_attachment" "lambda_permissions" {
   for_each = toset(local.managed_policies)
 
-  role       = aws_iam_role.lamda_role.name
+  role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/${each.value}"
+}
+
+# This block will create a Lambda function
+resource "aws_lambda_function" "lambda_polly_service" {
+  function_name = "TTStranslatorfunction"
+  role = aws_iam_role.lambda_role.arn
+  filename = data.archive_file.javascript_polly_lambda.output_path
+
+  runtime = "node.js22.x"
 }
