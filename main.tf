1cdb3ae79e6316c426f0bce0414b020a17c37e76
kai-generations <kai.newgenerations@gmail.com>
2026-01-28 18:20:00 -0500

    corrected runtime from node.js22.x to nodejs22.x

--- main.tf
+++ main.tf
@@ -56,5 +56,5 @@ resource "aws_lambda_function" "lambda_polly_service" {
   role          = aws_iam_role.lambda_role.arn
   filename      = data.archive_file.javascript_polly_lambda.output_path
 
-  runtime = "node.js22.x"
+  runtime = "nodejs22.x"
 }
