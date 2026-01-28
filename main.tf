f83a0dfad89fa39528f1d992236ad22ffabd0244
kai-generations <kai.newgenerations@gmail.com>
2026-01-27 19:21:18 -0500

    added locals block and policy attachment block

--- main.tf
+++ main.tf
@@ -2,8 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# The locals block is used to list which AWS policies will be used
+locals {
+  managed_policies = [
+    "AmazonPollyFullAccess",
+    "AmazonS3FullAccess",
+    "AWSLambdaBasicExecutionRole"
+  ]
+}
+
+# This resource creates the lambda role 
 resource "aws_iam_role" "lamda_role" {
   name = "PollyTranslationRole"
+  force_detach_policies = true
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -16,4 +27,12 @@ resource "aws_iam_role" "lamda_role" {
         }
     }]
   })
+}
+
+# This block attaches the managed policies to our lambda role
+resource "aws_iam_role_policy_attachment" "lambda_permissions" {
+  for_each = toset(local.managed_policies)
+  
+  role = aws_iam_role.lamda_role
+  policy_arn = "arn:aws:iam::aws:policy/${each.value}"
 }
